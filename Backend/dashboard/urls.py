f55b2e9d2f1a2206b6a6a1279e766e9a66d7ba73
archana  chourawar <archanachourawar1708@gmail.com>
2026-07-02 13:57:38 +0530

    Added Vendor and Customer Dashboard APIs :wq

---
+++ Backend/dashboard/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import VendorDashboardView, CustomerDashboardView
+
+urlpatterns = [
+
+    path(
+        'vendor/',
+        VendorDashboardView.as_view(),
+        name='vendor-dashboard'
+    ),
+
+    path(
+        'customer/',
+        CustomerDashboardView.as_view(),
+        name='customer-dashboard'
+    ),
+
+]
--- Backend/ecommerce/urls.py
+++ Backend/ecommerce/urls.py
@@ -1,28 +1,46 @@
-"""
-URL configuration for ecommerce project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+
+    # Accounts
     path('', include('accounts.urls')),
-    path('', include('payments.urls')),
-    path('', include('orders.urls')),
-    path('', include('products.urls')),
-    path('', include('categories.urls')),
-    path('',include('cart.urls'))
+
+    # Categories
+    path(
+        'api/categories/',
+        include('categories.urls')
+    ),
+
+    # Products
+    path(
+        'api/products/',
+        include('products.urls')
+    ),
+
+    # Orders
+    path(
+        'api/orders/',
+        include('orders.urls')
+    ),
+
+    # Cart
+    path(
+        'cart/',
+        include('cart.urls')
+    ),
+
+    # Payments
+    path(
+        '',
+        include('payments.urls')
+    ),
+
+    # Dashboard
+    path(
+        'api/dashboard/',
+        include('dashboard.urls')
+    ),
 ]
